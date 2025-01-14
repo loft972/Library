92096940f2bef4eec2b79a5b58f4e339e17fad44
loic.fontaine <lflolo972@gmail.com>
2025-01-14 19:22:35 +0100

    First commit

---
+++ libraryback/src/test/java/com/loft/libraryback/LibrarybackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loft.libraryback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarybackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
